11f19b4346311a95326376590c5afadea9c91281
muhammadisan <muhammadisan2015@gmail.com>
2021-02-09 00:20:25 +0700

    zzz

--- src/assignment/worker/worker/app.js
+++ src/assignment/worker/worker/app.js
@@ -3,22 +3,36 @@ const { store$ } = require('./store');
 const { addWorkerAsync, loadWorkersAsync } = require('./worker-client');
 
 const table = document.getElementById('table-worker');
-const thead = document.getElementsByTagName('tbody')[0].getElementsByTagName('tr')[0];
+const thead = document
+  .getElementsByTagName('tbody')[0]
+  .getElementsByTagName('tr')[0];
 const ttail = document.getElementById('add-worker-tr');
-// const form = document.getElementById('add-worker-form');
-console.log(ttail);
+const form = document.getElementById('form-worker');
+const input = document.getElementsByClassName('data-input');
 
-// form.onsubmit = (event) => {
-//   event.preventDefault();
-//   const worker = input.value;
-//   console.log(worker);
-//   if (!worker?.length) {
-//     return;
-//   }
-//   // dispatch action add
-//   store$.dispatch(addWorkerAsync(worker));
-//   input.value = '';
-// };
+form.onsubmit = (event) => {
+  event.preventDefault();
+  let worker = {
+    payload: {
+      id: input[0].value,
+      name: input[1].value,
+      address: input[2].value,
+      email: input[3].value,
+      nohp: input[4].value,
+      biografi: input[5].value,
+      photo: input[6].value,
+    },
+  };
+  console.log(worker);
+  if (!worker?.length) {
+    return;
+  }
+  // dispatch action add
+  store$.dispatch(addWorkerAsync(worker));
+  for (let i = 0; i < input.length; i++) {
+    input[i].value = '';
+  }
+};
 
 // presentation layer
 store$.subscribe(() => {
